5c944686f3ebd15f7cc40c4593bdfffaa49c1e3f
muxiao-123 <582775919@qq.com>
2022-11-18 01:08:45 +0800

    chore: modify base url

--- src/home/index.js
+++ src/home/index.js
@@ -1,14 +1,14 @@
 // eslint-disable-next-line no-unused-vars
-const baseUrl = 'http://192.168.43.217:8888/'
+// const baseUrl = 'http://192.168.43.217:8888/'
 const routerArr = [
-  { path: 'test-scroll/', params: '', name: '时间分片' },
-  { path: 'vitrual-list-scroll/', params: '', name: '虚拟列表' }
+  { path: '../test-scroll/', params: '', name: '时间分片' },
+  { path: '../vitrual-list-scroll/', params: '', name: '虚拟列表' }
 ]
 const generateRouter = (pathArr) => {
   const bodyEle = document.querySelector('.navigate-box')
   pathArr.forEach((v) => {
     const aEle = document.createElement('a')
-    aEle.setAttribute('href', baseUrl + v.path)
+    aEle.setAttribute('href', v.path)
     aEle.setAttribute('target', '_self')
     aEle.classList.add('navigate')
     aEle.innerText = v.name

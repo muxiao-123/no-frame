f89ef9432ebfd4fdd0b3d1a1e98bf8abb455f9d6
muxiao-123 <582775919@qq.com>
2022-11-17 14:43:02 +0800

    chore: init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const result = 'fds'
+function getValue(val) {
+  return val
+}
+setTimeout(() => {
+  console.log('ok')
+})
+
+for (let i = 0; i < 9; i++) {
+  console.log(i)
+}
+
+getValue(result)
